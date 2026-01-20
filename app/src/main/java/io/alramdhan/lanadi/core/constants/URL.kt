0cdf3d0dc16c8fa91faa15d7aaa4a28457fabfa2
Dika <al.ramdhan312@gmail.com>
2026-01-20 14:42:46 +0700

    add endpoint get product

--- app/src/main/java/io/alramdhan/lanadi/core/constants/URL.kt
+++ app/src/main/java/io/alramdhan/lanadi/core/constants/URL.kt
@@ -1,8 +1,9 @@
 package io.alramdhan.lanadi.core.constants
 
 object URL {
-    const val BASE_URL: String = "http://192.168.0.103:8000/"
+    const val BASE_URL: String = "http://192.168.1.11:8000/"
     const val ENDPOINT_LOGIN: String = "api/login"
     const val ENDPOINT_LOGOUT: String = "api/logout"
     const val ENDPOINT_GET_KATEGORI: String = "api/kategori/get"
+    const val ENDPOINT_GET_PRODUK: String = "api/produk/get-all"
 }
